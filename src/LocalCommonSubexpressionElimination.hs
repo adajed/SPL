dd1f40dbc321058e9cff5c45d931a5c9f28ad704
Adam Jędrych <adam.jedrych25@gmail.com>
2020-05-29 11:54:02 +0200

    [opt] Improve LCSE

--- src/LocalCommonSubexpressionElimination.hs
+++ src/LocalCommonSubexpressionElimination.hs
@@ -9,44 +9,53 @@ import OptimizationUtils
 
 localCommonSubexpressionElimination :: BBGraph -> BBGraph
 localCommonSubexpressionElimination g = g { ids = Map.map f (ids g) }
-    where f = liftBB (localCommonSubexprElim . memReadElim)
+    where f = liftBB (localCommonSubexprElim . memElim)
 
 localCommonSubexprElim :: [IR] -> [IR]
-localCommonSubexprElim xs = Prelude.foldl eliminate xs c
+localCommonSubexprElim xs = Prelude.foldl elim xs c
     where c = Prelude.filter isCommonSubExpr (allPairs xs)
+          elim xs ((n1, ir1), (n2, ir2)) =
+              case (takeVar ir1, takeVar ir2) of
+                (Just x, Just y) -> change n2 (IR_Ass y (VarIR x)) xs
+                _ -> xs
 
-memReadElim :: [IR] -> [IR]
-memReadElim xs = Prelude.foldl eliminate xs c
-    where c = Prelude.filter (isCommonMemRead xs) (allPairs xs)
+memElim :: [IR] -> [IR]
+memElim xs = Prelude.foldl eliminate xs c
+    where c = Prelude.filter (isCommonMem xs) (allPairs xs)
 
-isCommonMemRead :: [IR] -> (IR, IR) -> Bool
-isCommonMemRead xs (ir1@(IR_MemRead _ v), ir2@(IR_MemRead _ v')) = v == v' && noMemSave xs ir1 ir2
-isCommonMemRead _ _ = False
+isCommonMem :: [IR] -> ((Int, IR), (Int, IR)) -> Bool
+isCommonMem xs ((n1, IR_MemRead _ v),   (n2, IR_MemRead _ v')) = v == v' && noMemSave xs n1 n2
+isCommonMem xs ((n1, IR_MemSave v _ _), (n2, IR_MemRead _ v')) = v == v' && noMemSave xs n1 n2
+isCommonMem xs ((n1, IR_MemSave v _ _), (n2, IR_MemSave v' _ _)) = v == v' && noMemSave xs n1 n2
+isCommonMem _ _ = False
 
-noMemSave :: [IR] -> IR -> IR -> Bool
-noMemSave xs x1 x2 = h xs 0
-    where h [] _ = False
-          h (y:ys) 0 = h ys (if x1 == y then 1 else 0)
-          h ((IR_MemSave _ _ _):ys) 1 = False
-          h (y:ys) 1 = if y == x2 then True else h ys 1
+noMemSave :: [IR] -> Int -> Int -> Bool
+noMemSave xs n1 n2 = h (Prelude.take (n2 - n1 - 1) (Prelude.drop (n1+1) xs))
+    where h [] = True
+          h ((IR_MemSave _ _ _):_) = False
+          h ((IR_Call _ _ _):_) = False
+          h ((IR_VoidCall _ _):_) = False
+          h (y:ys) = h ys
 
-eliminate :: [IR] -> (IR, IR) -> [IR]
-eliminate xs (ir1, ir2) =
-    case (takeVar ir1, takeVar ir2) of
-      (Just x, Just y) ->
-          let newir = IR_Ass y (VarIR x)
-              f ir = if ir == ir2 then newir else ir
-           in Prelude.map f xs
-      _ -> xs
+eliminate :: [IR] -> ((Int, IR), (Int, IR)) -> [IR]
+eliminate xs ((n1, IR_MemRead x _), (n2, IR_MemRead y _)) = change n2 newir xs
+    where newir = IR_Ass y (VarIR x)
+eliminate xs ((n1, IR_MemSave _ _ _), (n2, IR_MemSave _ _ _)) = change n1 IR_Nop xs
+eliminate xs ((n1, IR_MemSave _ v _), (n2, IR_MemRead x _)) = change n2 newir xs
+    where newir = IR_Ass x v
+eliminate xs _ = xs
 
+change :: Int -> a -> [a] -> [a]
+change n x xs = (Prelude.take n xs) ++ [x] ++ (Prelude.drop (n+1) xs)
 
-allPairs :: [a] -> [(a, a)]
-allPairs xs = h xs []
-    where h [] acc = acc
+allPairs :: [a] -> [((Int, a), (Int, a))]
+allPairs xs = h (zip [0..] xs) []
+    where h :: [(Int, a)] -> [((Int, a), (Int, a))] -> [((Int, a), (Int, a))]
+          h [] acc = acc
           h (y:ys) acc = h ys ((zip (repeat y) ys) ++ acc)
 
-isCommonSubExpr :: (IR, IR) -> Bool
-isCommonSubExpr (IR_BinOp op _ v1 v2, IR_BinOp op' _ v1' v2') =
+isCommonSubExpr :: ((Int, IR), (Int, IR)) -> Bool
+isCommonSubExpr ((_, IR_BinOp op _ v1 v2), (_, IR_BinOp op' _ v1' v2')) =
     op == op' && v1 == v1' && v2 == v2'
-isCommonSubExpr (IR_UnOp op _ v, IR_UnOp op' _ v') = op == op' && v == v'
+isCommonSubExpr ((_, IR_UnOp op _ v), (_, IR_UnOp op' _ v')) = op == op' && v == v'
 isCommonSubExpr _ = False
